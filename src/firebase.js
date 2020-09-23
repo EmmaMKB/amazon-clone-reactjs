83ee50910272b1bfdbb2d59af4f3166a64884afd
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-09-23 16:04:46 -0700

    ✨ Add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDYW3sNZibGCA8k_OnTA6ES5gwh2UEhUuc",
+  authDomain: "clone-6393c.firebaseapp.com",
+  databaseURL: "https://clone-6393c.firebaseio.com",
+  projectId: "clone-6393c",
+  storageBucket: "clone-6393c.appspot.com",
+  messagingSenderId: "1048389329731",
+  appId: "1:1048389329731:web:bba5e40a0abeb2a74e45f6",
+  measurementId: "G-8K21HRVRVE"
+};
