90594ac5bcea9ddf0af07ffabd093c0bf86b1378
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-09-23 16:26:18 -0700

    ✨ Implement Header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,27 @@ import React from "react";
 const Header = () => {
     return(
         <div className="header">
-
+            <img className='header__logo'
+                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                 alt="amazon logo"
+            />
+            <div className="header__search">
+                <input type="text" className="header__searchINput"/>
+            </div>
+            <div className="header__nav">
+                <div className="header__navIten">
+                    <span className='navItem__firstLine'>Hello</span>
+                    <span className='navItem__firstLine'>Sign In</span>
+                </div>
+                <div className="header__navIten">
+                    <span className='navItem__firstLine'>Return</span>
+                    <span className='navItem__firstLine'>& Orders</span>
+                </div>
+                <div className="header__navIten">
+                    <span className='navItem__firstLine'>Your</span>
+                    <span className='navItem__firstLine'>Prime</span>
+                </div>
+            </div>
         </div>
     )
 }
