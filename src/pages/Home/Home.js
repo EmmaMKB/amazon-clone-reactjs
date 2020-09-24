35a5b06b44616b587c6028fc9dd8efde05e03f7f
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-09-24 16:56:04 -0700

    ✨ Add product id field

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -13,12 +13,14 @@ const Home = () => {
                 />
                 <div className="home__row">
                     <Product
+                        id={1}
                         title="The lean StartUp"
                         price={99.9}
                         image={require("../../assets/images/p1.jpg")}
                         rating={4}
                     />
                     <Product
+                        id={2}
                         title="Cool girl nike shoes"
                         price={39.9}
                         image={require("../../assets/images/p2.jpg")}
@@ -27,18 +29,21 @@ const Home = () => {
                 </div>
                 <div className="home__row">
                     <Product
+                        id={3}
                         title="Sewing Bemina Sewing machine"
                         price={129.9}
                         image={require("../../assets/images/p3.jpg")}
                         rating={4}
                     />
                     <Product
+                        id={4}
                         title="SKMei watch"
                         price={9.99}
                         image={require("../../assets/images/p4.jpg")}
                         rating={2}
                     />
                     <Product
+                        id={5}
                         title="Louis Vuitton shoes"
                         price={129.9}
                         image={require("../../assets/images/p5.jpg")}
@@ -47,6 +52,7 @@ const Home = () => {
                 </div>
                 <div className="home__row">
                     <Product
+                        id={6}
                         title="Eloquent Javascript"
                         price={19.9}
                         image={require("../../assets/images/p6.png")}
