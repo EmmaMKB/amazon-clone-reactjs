70f5b7a4307e544e94a28b8b91fcb9dd6d486d16
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-09-23 17:22:11 -0700

    ✨ Add Home page in App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Home from "./pages/Home/Home";
 import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Home />
     </div>
   );
 }
