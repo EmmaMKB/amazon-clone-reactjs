72e8eea67da083385ca88092c5b09531b5670c08
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-09-23 18:01:04 -0700

    ✨ Add banner in home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,7 +4,13 @@ import "./home.css"
 const Home = () => {
     return(
         <div className="home">
-
+            <div className="home__container">
+                <img
+                    src={require("../../assets/images/amazon-banner.jpg")}
+                    alt=""
+                    className="home__image"
+                />
+            </div>
         </div>
     )
 }
