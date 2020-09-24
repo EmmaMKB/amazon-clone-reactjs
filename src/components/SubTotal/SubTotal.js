d609b426f87cd43c25036e5debe9d0d80b7dd367
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-09-24 15:42:49 -0700

    ✨ Implement SubTotal component

--- src/components/SubTotal/SubTotal.js
+++ src/components/SubTotal/SubTotal.js
@@ -1,10 +1,28 @@
 import React from "react";
 import "./subtotal.css";
+import CurrencyFormat from "react-currency-format";
 
 const SubTotal = () => {
     return(
         <div className="subtotal">
-
+            <CurrencyFormat
+                renderText={(value) => (
+                    <>
+                        <p>
+                            Subtotal (0 items): <strong>{value}</strong>
+                        </p>
+                        <small className='subtotal__gift'>
+                            <input type="checkbox" /> This order contains a gift
+                        </small>
+                    </>
+                )}
+                decimalScale={2}
+                value={20.2}
+                displayType="text"
+                thousandSeparator={true}
+                prefix="$"
+            />
+            <button>Proceed to checkout</button>
         </div>
     )
 }
