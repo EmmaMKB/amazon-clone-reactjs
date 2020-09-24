bde2537d2a1f9d29baa7c835948a2ae4bf5cb17e
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-09-23 17:15:19 -0700

    🎨 Add style and icons in Header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,7 @@
+import "./header.css";
 import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 const Header = () => {
     return(
@@ -8,21 +11,26 @@ const Header = () => {
                  alt="amazon logo"
             />
             <div className="header__search">
-                <input type="text" className="header__searchINput"/>
+                <input type="text" className="header__searchInput"/>
+                <SearchIcon className='header__searchIcon'/>
             </div>
             <div className="header__nav">
                 <div className="header__navIten">
                     <span className='navItem__firstLine'>Hello</span>
-                    <span className='navItem__firstLine'>Sign In</span>
+                    <span className='navItem__secondLine'>Sign In</span>
                 </div>
                 <div className="header__navIten">
                     <span className='navItem__firstLine'>Return</span>
-                    <span className='navItem__firstLine'>& Orders</span>
+                    <span className='navItem__secondLine'>& Orders</span>
                 </div>
                 <div className="header__navIten">
                     <span className='navItem__firstLine'>Your</span>
-                    <span className='navItem__firstLine'>Prime</span>
+                    <span className='navItem__secondLine'>Prime</span>
                 </div>
+            </div>
+            <div className="header__basket">
+                <ShoppingBasketIcon />
+                <span className='navItem__secondLine header__basketCount'>0</span>
             </div>
         </div>
     )
