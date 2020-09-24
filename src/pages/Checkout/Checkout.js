dff1fc8e0a49f249b08f0ac4e28ce98b5dae5433
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-09-24 14:11:05 -0700

    ✨ Add Checkout page component

---
+++ src/pages/Checkout/Checkout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./checkout.css";
+
+const Checkout = () => {
+    return(
+        <div className="checkout__page">
+
+        </div>
+    )
+}
+
+export default Checkout;
