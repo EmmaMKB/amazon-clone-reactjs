04480ba67f0f39ec1f0d24124205274b2738e80f
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-09-24 16:55:50 -0700

    ✨ Read basket from state

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,11 @@ import React from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import {Link} from "react-router-dom";
+import { useStateValue } from "../../context/StateProvider";
 
 const Header = () => {
+    const [{basket}, dispatch] = useStateValue();
+
     return(
         <div className="header">
             <Link to='/'>
@@ -34,7 +37,7 @@ const Header = () => {
             <Link to='/checkout'>
                 <div className="header__basket">
                     <ShoppingBasketIcon />
-                    <span className='navItem__secondLine header__basketCount'>0</span>
+                    <span className='navItem__secondLine header__basketCount'>{basket.length}</span>
                 </div>
             </Link>
         </div>
