b879d6063910a76df238c55fa9cc5c95a8170610
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-09-24 15:01:53 -0700

    ✨ Add SubTotal component

---
+++ src/components/SubTotal/SubTotal.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./subtotal.css";
+
+const SubTotal = () => {
+    return(
+        <div className="subtotal">
+
+        </div>
+    )
+}
+export default SubTotal;
