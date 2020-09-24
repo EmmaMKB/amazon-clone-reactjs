e69a30c6495b507b29036038dd26ded41ca915a1
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-09-24 12:50:05 -0700

    ✨ Implement Product component

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,9 +1,22 @@
 import React from "react";
+import "./product.css";
 
 const Product = () => {
     return(
-        <div className="product-card">
+        <div className="product__card">
+            <div className="product__info">
+                <p>The Lean startup Home Context</p>
+                <p className="product__price">
+                    <small>$</small>
+                    <strong>99.9</strong>
+                </p>
+                <div className="product__rating">
+                    <p>⭐</p>
+                </div>
+            </div>
+            <img src={require("../../assets/images/p1.jpg")} alt=""/>
 
+            <button>Add to Basket</button>
         </div>
     )
 }
