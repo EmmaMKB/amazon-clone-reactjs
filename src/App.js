4f8253ae290e8076da5045f8bbc6d675787401f8
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-09-24 14:12:10 -0700

    ✨ Edit routing

--- src/App.js
+++ src/App.js
@@ -7,13 +7,12 @@ function App() {
   return (
       <Router>
           <div className="app">
+              <Header />
               <Switch>
                   <Route path="/checkout">
-                      <Header />
                       <h1>checkout</h1>
                   </Route>
                   <Route path="/">
-                      <Header />
                       <Home />
                   </Route>
               </Switch>
