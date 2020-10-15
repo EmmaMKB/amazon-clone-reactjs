f09de6c01cc95adfb312a2b743d02046a981ef5c
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-10-15 16:26:38 -0700

    ✨ Implement CheckoutProduct component

--- src/components/CheckoutProduct/CheckoutProduct.js
+++ src/components/CheckoutProduct/CheckoutProduct.js
@@ -1,11 +1,27 @@
 import React from "react";
 import "./checkoutProduct.css";
 
-const CheckoutProduct = () => {
+const CheckoutProduct = ({ id, image, title, price, rating }) => {
 
     return(
         <div className="checkoutProduct">
-
+            <img className="checkoutProduct__image" src={require(image)} alt={title} />
+            <div className="checkoutProduct__info">
+                <p className="checkoutProduct__title">{title}</p>
+                <p className="checkoutProduct__price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="checkoutProduct__rating">
+                    {Array(rating)
+                        .fill()
+                        .map((_, i) => (
+                            <span key={i} role='img' aria-label="star">⭐</span>
+                        ))
+                    }
+                </div>
+                <button>Remove from basket</button>
+            </div>
         </div>
     )
 }
