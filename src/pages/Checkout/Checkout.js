8067c17a57bbca70ff8cb58969a736ff6cc6760e
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-10-15 16:34:05 -0700

    ✨ Show baskets product on Checkout page

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./checkout.css";
 import SubTotal from "../../components/SubTotal/SubTotal";
+import {useStateValue} from "../../context/StateProvider";
+import CheckoutProduct from "../../components/CheckoutProduct/CheckoutProduct";
 
 const Checkout = () => {
+    const [{basket}, dispatch] = useStateValue();
     return(
         <div className="checkout__page">
             <div className="checkout__left">
@@ -13,6 +16,19 @@ const Checkout = () => {
                 />
                 <div>
                     <h2 className="checkout__title">Your shopping Basket</h2>
+
+                    {
+                        basket.map((item, index) => (
+                            <CheckoutProduct
+                                price={item.price}
+                                rating={item.rating}
+                                image={item.image}
+                                title={item.title}
+                                id={item.id}
+                                key={index}
+                            />
+                        ))
+                    }
                 </div>
             </div>
             <div className="checkout__right">
