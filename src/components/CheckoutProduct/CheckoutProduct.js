c6828e5c4429dab1ef3d6b3def0cb8537190f808
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-10-15 17:10:51 -0700

    ✨ handle remove from Basket

--- src/components/CheckoutProduct/CheckoutProduct.js
+++ src/components/CheckoutProduct/CheckoutProduct.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./checkoutProduct.css";
+import {useStateValue} from "../../context/StateProvider";
 
 const CheckoutProduct = ({ id, image, title, price, rating }) => {
+    const [{basket}, dispatch] = useStateValue();
 
+    const removeFromBasket = () => {
+        dispatch({
+            type: "REMOVE_FROM_BASKET",
+            id: id,
+        })
+    }
     return(
         <div className="checkoutProduct">
             <img className="checkoutProduct__image" src={image} alt={title} />
@@ -20,7 +28,7 @@ const CheckoutProduct = ({ id, image, title, price, rating }) => {
                         ))
                     }
                 </div>
-                <button>Remove from basket</button>
+                <button onClick={removeFromBasket}>Remove from basket</button>
             </div>
         </div>
     )
